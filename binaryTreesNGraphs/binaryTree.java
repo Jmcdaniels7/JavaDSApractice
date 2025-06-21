ea84abf5a72e9d3bd0cf0d798cd5d9e403b7f8e6
Jamcda <jamcdaniels99@gmail.com>
2025-06-21 13:54:09 -0400

    added recursive insert, inorder traversal, started preorder traversal, added a visual of tree with inorder traversal caption.

--- binaryTreesNGraphs/binaryTree.java
+++ binaryTreesNGraphs/binaryTree.java
@@ -1,6 +1,6 @@
 
-
-public class binaryTree<T> {
+// we add Comparable<T> to be able to compare templated types
+public class binaryTree<T extends Comparable <T>> {
     
     public static class Node<T> {
         public T data;
@@ -16,5 +16,68 @@ public class binaryTree<T> {
 
     }
 
-    
+    Node<T> root;
+
+    binaryTree() {
+        root = null;
+    }
+
+    public void insert(T value)
+    {
+        root = recursInsert(root, value);
+
+    }
+
+    //utilizing recursion we either create a root or we add to the left or right node 
+    private Node<T> recursInsert(Node<T> root, T value)
+    {
+        //creates a root
+        if(root == null)
+        {
+            root = new Node<T>(value);
+            return root;
+        }
+
+        // we recursively call an insert to manage if we want a new left or right node
+        // left is less than current node
+        if(value.compareTo(root.data) < 0)
+        {
+            root.left = recursInsert(root.left, value);
+        }
+        //right is greater than current node
+        else if(value.compareTo(root.data) > 0)
+        {
+            root.right = recursInsert(root.right, value);
+        }
+
+        return root;
+
+    }
+
+    public void inorderTrav(Node<T> node)
+    {
+        inorderRecurs(root);
+        System.out.println();
+    }
+
+    //this goes as deep as we can to the left and then goes back up to each node and 
+    //asks if there is a right node to visit until we reach the root and then we do 
+    //the same on the right side
+    private void inorderRecurs(Node<T> node) {
+        if (node != null) {
+            inorderRecurs(node.left);
+            System.out.print(node.data + " -> ");
+            inorderRecurs(node.right);
+        }
+    }
+
+    public void preOrderTrav(Node<T> node)
+    {
+        preorderRecurs(node);
+    }
+
+    private void preorderRecurs(Node<T> node)
+    {
+        
+    }
 }
--- binaryTreesNGraphs/solution.java
+++ binaryTreesNGraphs/solution.java
@@ -4,6 +4,40 @@ import java.util.*;
 public class solution {
     public static void main(String args[]) {
 
+        binaryTree<Integer> tree = new binaryTree<Integer>();
+
+        tree.insert(57);
+        tree.insert(75);
+        tree.insert(58);
+        tree.insert(89);
+        tree.insert(17);
+        tree.insert(21);
+        tree.insert(25);
+        tree.insert(12);
+
+        tree.inorderTrav(tree.root);
+
+        /* current tree
+                  57
+               /      \
+            17          75
+          /   \        /   \
+        12    21      58    89
+                \
+                 25
+         
+         
+         Inorder Traverse:
+          DFS to the left most node and comes back up printingeach left node and 
+          checks if there is a right node to do dsf to that subtreeuntil it reaches 
+          the root node where then it does the same process to the left most nodes 
+          of the right side of the tree
+        */
+
+        
+
+        
+
     
     }
 
